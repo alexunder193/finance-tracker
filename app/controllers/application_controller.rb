d134af799214474ffd0e94813b66fbded0f8444b
Alex Kyriakou <akyriakou@skroutz.gr>
2021-11-26 20:20:26 +0200

    Create name surname form in signup and edit user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # If we want to add extra field to devise gem like first_name and last_name user edit or create form.
+  # When submit form we need to add the above in order to save to DB table
+  # -----------------------------------------------------------------------
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+  # -----------------------------------------------------------------------
 end
